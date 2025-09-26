2258ee08884f8a847242d87e8c325bcbcf6cdd79
Thaiduc1611 <dangthaiduca10k55@gmail.com>
2025-09-26 21:45:48 +0700

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace bai4
+namespace bai3
 {
     internal static class Program
     {
@@ -16,7 +16,7 @@ namespace bai4
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmpheptinh());
+            Application.Run(new Frmcombobox());
         }
     }
 }
